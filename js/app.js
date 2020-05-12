c66cd04c0ead1558e7f014aaf8cbc66d68143bb0
cabrucas <60257049+cabrucas@users.noreply.github.com>
2020-05-12 09:45:46 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//main slider init
+var mySwiper = new Swiper ('.mainpage-slider', {
+    direction: 'horizontal',
+    speed: 1500,
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+        delay: 1500,
+    },
+    disableOnInteraction: false,
+})
