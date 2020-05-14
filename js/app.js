a2b4df27413a75e47f48fda13ac7dc2b16516e86
cabrucas <60257049+cabrucas@users.noreply.github.com>
2020-05-14 13:55:19 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -9,3 +9,33 @@ var mySwiper = new Swiper ('.mainpage-slider', {
     },
     disableOnInteraction: false,
 })
+// анимации при скролле
+sal({
+    threshold: 0.01,
+    once: false,
+});
+
+// плавный скролл
+SmoothScroll({
+    animationTime    : 1000, // [ms]
+    stepSize         : 100, // [px]
+    accelerationDelta : 20,  // 50
+    accelerationMax   : 2,   // 3
+    keyboardSupport   : true,  // option
+    arrowScroll       : 50,    // [px]
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
+  
+    touchpadSupport   : false,
+    fixedBackground   : true, 
+    excluded          : ''    
+  });
+// параллакс 
+var rellax = new Rellax('.rellax', {
+    center: true,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
